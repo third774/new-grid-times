43f110c6ffe4032dd23eb31b51875280797c246e
Kevin Kipp <kkipp@cloudflare.com>
2021-08-16 00:28:36 -0500

    Excercise 1: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <TabletAndUpMenuButtons>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </TabletAndUpMenuButtons>
         <Logo />
+        <SubscriberButtons>
+          <Button>Subscribe</Button>
+          <AlreadySubscriberButton>
+            Already a subscriber?
+          </AlreadySubscriberButton>
+        </SubscriberButtons>
       </MainHeader>
     </header>
   );
@@ -39,6 +53,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +77,55 @@ const ActionGroup = styled.div`
   }
 `;
 
+const TabletAndUpActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const TabletAndUpMenuButtons = styled(TabletAndUpActionGroup)`
+  width: 124px;
+`;
+
+const SubscriberButtons = styled(TabletAndUpActionGroup)`
+  justify-self: end;
+  align-self: center;
+  position: relative;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: block;
+  }
+`;
+
+const AlreadySubscriberButton = styled.a`
+  position: absolute;
+  text-decoration: underline;
+  font-style: italic;
+  font-size: 0.75rem;
+  text-align: center;
+  width: 100%;
+  margin-top: 8px;
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  display: grid;
+  place-items: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    place-items: revert;
+    grid-template-columns: 1fr auto 1fr;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
 `;
 
 export default Header;
